6dae3457d01665064a80d2689845cc198fed61ce
Yevgeny <Yevgeny.Lavrov@yandex.com>
2023-03-05 22:37:24 -0500

    Add basic user registration

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")] // POST: api/account/register
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDTO.username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.password)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}

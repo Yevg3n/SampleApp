3849e8e103d102d89fbe77e2df8eece14bf9b085
Yevgeny <Yevgeny.Lavrov@yandex.com>
2023-03-06 12:56:10 -0500

    Check if user already exists in DB

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -18,18 +19,27 @@ namespace API.Controllers
         [HttpPost("register")] // POST: api/account/register
         public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
         {
+            if(await UserExists(registerDTO.Username)) return BadRequest("Username already exists");
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName = registerDTO.username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.password)),
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            // TODO: Clean this one up once complete & tested
+            bool exists = await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return exists;
         }
     }
 }

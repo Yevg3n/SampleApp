0d84c26fab2770f0c4fafa0fdb0708e250c3d31e
Yevgeny <Yevgeny.Lavrov@yandex.com>
2023-03-14 16:57:10 -0400

    Inject account service into Login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,9 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-  model: any = {}
-  
+  model: any = {};
+  loggedIn = false;
+
+  constructor(private accountService: AccountService){}
+
   login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
     console.log(this.model);
   }
 
